35e47c4ddbb17ed7c2bd4cb40ea7b44f08e61ac3
Bradley Smith <bradleysmith93@gmail.com>
2023-12-02 14:42:47 +1100

    Prettier: Initial run

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,24 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 const bodyClassName = inter.className + " dark";
 
 export const metadata: Metadata = {
-  title: 'Advent of Code 2023',
-  description: 'Advent of Code 2023',
-}
+  title: "Advent of Code 2023",
+  description: "Advent of Code 2023",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={bodyClassName}>{children}</body>
     </html>
-  )
+  );
 }
