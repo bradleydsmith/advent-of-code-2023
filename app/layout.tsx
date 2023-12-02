4273f7e03e06d2c16e225446333dc524aad15d51
Bradley Smith <bradleysmith93@gmail.com>
2023-12-02 19:36:56 +1100

    Header: Add header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={bodyClassName}>{children}</body>
+      <body className={bodyClassName}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
