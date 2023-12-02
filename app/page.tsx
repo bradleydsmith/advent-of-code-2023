96a4229e6e7a6e5a3e91d6ea2a1e0202b76626b3
Bradley Smith <bradleysmith93@gmail.com>
2023-12-02 19:51:05 +1100

    Home: Update home page with github link and further header text

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,24 @@ export default function Home() {
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         Advent Of Code 2023
       </h1>
+      <h2 className="pt-4 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+        Solving the Advent of Code 2023 puzzles with Typescript and NextJS
+      </h2>
+      <p className="leading-7 [&:not(:first-child)]:mt-6">
+        Source available on GitHub:
+        <br />
+        <a
+          className="hover:underline"
+          href="https://github.com/bradleydsmith/advent-of-code-2023"
+        >
+          https://github.com/bradleydsmith/advent-of-code-2023
+        </a>
+      </p>
       <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
         <li>
-          <Link href="/days/1">Day 1</Link>
+          <Link className="hover:underline" href="/days/1">
+            Day 1
+          </Link>
         </li>
       </ul>
     </main>
